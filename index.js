5d2a069e90fbfac855f640a9b347457563d4df8d
Rafael Quevedo <rafaelrpq@gmail.com>
2023-06-05 13:43:50 -0300

    ajustes css, erros, progresso

--- index.js
+++ index.js
@@ -12,15 +12,20 @@
 let dados = {}
 let host = 'https://grandepremio.com.br/programacao-da-tv'
 async function scrap (host) {
-    let url = 'https://api.api-ninjas.com/v1/webscraper?user_agent=html&url='+host
-    let req = await fetch (url,
-        {
-            headers: {
-                'X-Api-Key': '4GKo0wVXx2akBPaGTwyycQ==CsMYftLxfH2HIOKv'
-            },
-        }
-    )
-    dados = await req.json ();
+    let url = 'https://api.api-ninjas.com/v1/webscraper?url='+host
+
+    try {
+        let req = await fetch (url,
+            {
+                headers: {
+                    'X-Api-Key': '4GKo0wVXx2akBPaGTwyycQ==CsMYftLxfH2HIOKv'
+                },
+            }
+        )
+        dados = await req.json ();
+    } catch (e) {
+        main.innerHTML = e
+    }
 }
 
 scrap (host)
@@ -29,25 +34,39 @@ let html;
 let atual;
 let titulos;
 let tabelas;
-let header = document.querySelector ('header')
+let header = document.querySelector ('header span')
 let main = document.querySelector ('main')
 
 setTimeout (() => {
-    html  = parser.parseFromString (dados.data, 'text/html')
-    atual = html.querySelectorAll ('a.programacao-tv')[0].getAttribute ('href')
-    scrap (atual)
-    setTimeout ( () => {
+    try {
         html  = parser.parseFromString (dados.data, 'text/html')
-        titulos = html.querySelectorAll ('h2 strong, h3 strong')
-        tabelas = html.querySelectorAll ('table')
-        main.innerHTML = ''
+        atual = html.querySelectorAll ('a.programacao-tv')[0].getAttribute ('href')
+    } catch (e) {
+        header.innerHTML = 'Falha ao buscar dados!'
+        main.innerHTML = e
+        return ;
+    }
 
-        tabelas.forEach ((tabela, i) => {
-            main.innerHTML+= `<span>${titulos[i].innerText}</span>`
-            main.append (tabela)
-        })
+    scrap (atual)
 
-        header.innerHTML = 'Grande Prêmio - Programação do Fim de Semana'
+    setTimeout ( () => {
+        try {
+            html  = parser.parseFromString (dados.data, 'text/html')
+            titulos = html.querySelectorAll ('h2 strong, h3 strong')
+            tabelas = html.querySelectorAll ('table')
+            main.innerHTML = ''
+
+            tabelas.forEach ((tabela, i) => {
+                main.innerHTML+= `<span>${titulos[i].innerText}</span>`
+                main.append (tabela)
+            })
+            header.innerHTML = 'Grande Prêmio - Programação do Fim de Semana'
+
+        } catch (e) {
+            header.innerHTML = 'Falha ao buscar dados!'
+            main.innerHTML = e
+            return;
+        }
     },5000)
 }, 5000)
 
