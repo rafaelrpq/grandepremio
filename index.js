796536ef77a43d57e6276d9c1632f1d36f41b9ea
Rafael Quevedo <rafaelrpq@gmail.com>
2024-09-17 19:39:15 -0300

    testando gemini action

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function scrap (host) {
     }
 }
 
-scrap (host)
+
 let parser = new DOMParser ()
 let html;
 let atual;
@@ -37,51 +37,56 @@ let tabelas;
 let header = document.querySelector ('header span')
 let main = document.querySelector ('main')
 
-setTimeout (() => {
-    try {
-        html  = parser.parseFromString (dados.data, 'text/html')
-        atual = html.querySelectorAll ('a.programacao-tv')[0].getAttribute ('href')
-    } catch (e) {
-        header.innerHTML = 'Falha ao buscar dados!'
-        main.innerHTML = e+'<br>'
-        main.innerHTML += '<p>Atualize a página ou tente novamente em alguns instantes</p>'
-        return ;
-    }
 
-    scrap (atual)
-
-    setTimeout ( () => {
+scrap (host).then (() => { 
+    setTimeout (() => {
         try {
             html  = parser.parseFromString (dados.data, 'text/html')
-            titulos = html.querySelectorAll ('h2 strong, h3 strong')
-            tabelas = html.querySelectorAll ('table')
-            main.innerHTML = ''
-
-            tabelas.forEach ((tabela, i) => {
-                main.innerHTML+= `<span>${titulos[i].innerText}</span>`
-                main.append (tabela)
-            })
-            header.innerHTML = 'Grande Prêmio - Programação do Fim de Semana'
-
+            
+            atual = html.querySelectorAll ('a.programacao-tv')[0].getAttribute ('href')
+            
         } catch (e) {
-            header.innerHTML = 'Falha ao buscar dados!'
-            main.innerHTML = e
-            return;
+            header.innerHTML = 'Falha ao a.programacao-tv!'
+            main.innerHTML = e+'<br>'
+            main.innerHTML += '<p>Atualize a página ou tente novamente em alguns instantes</p>'
+            return ;
         }
-    },5000)
-}, 5000)
 
-
-if ('serviceWorker' in navigator) {
-    window.addEventListener ('load', () => {
-        navigator.serviceWorker.register ('sw.js')
-        .then (reg => {
-            console.log ('registrado!')
-            console.log (reg)
+    // scrap (atual)
+    
+    // setTimeout ( () => {
+        //     try {
+            //         html  = parser.parseFromString (dados.data, 'text/html')
+            //         titulos = html.querySelectorAll ('h2 strong, h3 strong')
+            //         tabelas = html.querySelectorAll ('table')
+            //         main.innerHTML = ''
+            
+            //         tabelas.forEach ((tabela, i) => {
+                //             main.innerHTML+= `<span>${titulos[i].innerText}</span>`
+                //             main.append (tabela)
+                //         })
+                //         header.innerHTML = 'Grande Prêmio - Programação do Fim de Semana'
+                
+                //     } catch (e) {
+                    //         header.innerHTML = 'Falha ao buscar dados!'
+                    //         main.innerHTML = e
+                    //         return;
+                    //     }
+                    // },5000)
+                }, 5000)
+                
+                
+        if ('serviceWorker' in navigator) {
+            window.addEventListener ('load', () => {
+            navigator.serviceWorker.register ('sw.js')
+            .then (reg => {
+                console.log ('registrado!')
+                console.log (reg)
+            })
+            .catch (err => {
+                console.log ('falha ao registrar')
+                console.log (err)
+            })
         })
-        .catch (err => {
-            console.log ('falha ao registrar')
-            console.log (err)
-        })
-    })
-}
+    }
+})
