9648808dc5197dda1b784af84cd742d4b314c21c
Rafael Quevedo <31359531+rafaelrpq@users.noreply.github.com>
2024-09-17 17:46:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@
 //     console.error ('error => ',e)
 // })
 
+const github = {{ github | json }};
+console.log(github.env.GITHUB_TOKEN);
+
 let dados = {}
 let host = 'https://grandepremio.com.br/programacao-da-tv'
 async function scrap (host) {
